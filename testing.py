033c0cf3609c0312cd17007b7a7424992d6dbe74
Frank Meier <frank_meier@datacomm.ch>
2018-04-05 08:00:11 +0200

    revert to pixel axis normalization

--- testing.py
+++ testing.py
@@ -20,6 +20,13 @@ def plot_pics(imgs):
     # plt.show()
 
 
+def convertToOneHot(vector, num_classes=None):
+    result = np.zeros((len(vector), num_classes), dtype='int32')
+    print(np.shape(result))
+    result[np.arange(len(vector)), vector] = 1
+    return result
+
+
 lables = {}
 inv_lables = {}
 X_train_raw, Y_train_raw, inv_lables_train = load_data.load_data_cached(load_data.DATA_PATH_TRAIN, lables)
@@ -27,18 +34,24 @@ X_valid_raw, Y_valid_raw, inv_lables_valid = load_data.load_data_cached(load_dat
 inv_lables.update(inv_lables_train)
 inv_lables.update(inv_lables_valid)
 
-# X_mean = np.mean(X_train_raw, axis=0)
-# X_std = np.std(X_train_raw, axis=0)
+# Normalization of the training and validationset.
+X_mean = np.mean(X_train_raw, axis=0)
+X_std = np.std(X_train_raw, axis=0)
+
+#X_train = (X_train_raw - X_mean) / (X_std + 0.0001)
+#X_valid = (X_valid_raw - X_mean) / (X_std + 0.0001)
+#Y_train = convertToOneHot(Y_train_raw, len(lables))
+#Y_valid = convertToOneHot(Y_valid_raw, len(lables))
 
 # normalize each (channel of each) picture
-img_rows, img_cols = 100, 100
-img_chan = 3
-input_shape = (img_rows, img_cols, img_chan)
-x_mean = np.ndarray(img_chan)
-x_std = np.ndarray(img_chan)
-for i in range(img_chan):
-    x_mean[i] = np.mean(np.array(X_train_raw)[:, :, :, i])
-    x_std[i] = np.std(np.array(X_train_raw)[:, :, :, i]) + 0.0001
+# img_rows, img_cols = 100, 100
+# img_chan = 3
+# input_shape = (img_rows, img_cols, img_chan)
+# x_mean = np.ndarray(img_chan)
+# x_std = np.ndarray(img_chan)
+# for i in range(img_chan):
+#     x_mean[i] = np.mean(np.array(X_train_raw)[:, :, :, i])
+#     x_std[i] = np.std(np.array(X_train_raw)[:, :, :, i]) + 0.0001
 
 imgs_raw = []
 
@@ -54,15 +67,22 @@ print(np.shape(imgs_raw[0]))
 plot_pics(imgs_raw)
 
 
-#imgs = (imgs_raw - X_mean) / (X_std + 0.0001)
-imgs = (imgs_raw - np.full(input_shape, x_mean)) / np.full(input_shape, x_std)
+imgs = (imgs_raw - X_mean) / (X_std + 0.0001)
+# imgs = (imgs_raw - np.full(input_shape, x_mean)) / np.full(input_shape, x_std)
 
 
-#model = keras.models.load_model('./Checkpoints/model_cnn_large_aug_dout_bn_test1/weights_epoch_010-0.02.hdf5')
 #model = keras.models.load_model('./Checkpoints/model_cnn_simple_test2/weights_epoch_025-0.03.hdf5')
 #model = keras.models.load_model('./Checkpoints/model_cnn_large_bn_dout_test2/weights_epoch_020-0.012.hdf5')
-#model = keras.models.load_model('./Checkpoints/model_cnn_large_aug_bn_dout_test3/weights_epoch_085-0.053.hdf5')
-model = keras.models.load_model('./Checkpoints/model_cnn_large_aug_bn_dout_test4/weights_epoch_025-0.115.hdf5')
+model = keras.models.load_model('./Checkpoints/model_cnn_large_aug_bn_dout_test3/weights_epoch_085-0.053.hdf5')
+model.summary()
+
+# check model on train data
+#print("CHECK MODEL:")
+#preds = model.predict(X_train)
+#print("Acc = ", np.sum(np.argmax(Y_train, axis=1) == np.argmax(preds, axis=1)) / len(preds))
+#preds = model.predict(X_valid)
+#print("VAL_Acc = ", np.sum(np.argmax(Y_valid, axis=1) == np.argmax(preds, axis=1)) / len(preds))
+
 
 preds = model.predict(imgs)
 
